27238d3329677ad220f4aac6ac9c61ea0b5858ca
elmerfds <22656503+elmerfds@users.noreply.github.com>
2024-12-29 16:45:50 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@
 const diceConfig = {
     dice1: {
         name: 'Color',
-        faces: ['Red', 'Green', 'White', 'Blue', 'Gold'],
+        faces: ['Red', 'Green', 'Gold'],
         default: 'Red',
         currentValue: null
     },
     dice2: {
         name: 'Pattern', 
-        faces: ['Striped', 'Spotty', 'Zigzag', 'Plain', 'Checkered'],
+        faces: ['Striped', 'Spotty', 'Plain'],
         default: 'Plain',
         currentValue: null
     },
