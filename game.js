5d51da0247d4ced9152de5be6e9eb1bb1b937d7a
elmerfds <22656503+elmerfds@users.noreply.github.com>
2024-12-29 03:40:27 +0000

    Create game.js

---
+++ game.js
@@ -0,0 +1,9 @@
+// game.js
+let foundPatterns = new Set();
+
+function generateAllCombinations() {
+    const combinations = [];
+    diceConfig.dice1.faces.forEach(color => {
+        diceConfig.dice2.faces.forEach(pattern => {
+            diceConfig.dice3.faces.forEach(decoration => {
+                combinations.push
